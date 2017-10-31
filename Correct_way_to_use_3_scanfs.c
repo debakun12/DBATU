5896ab634efa8ecc2237917e2a6c6ec76c84b4ad
debakun12 <31770244+debakun12@users.noreply.github.com>
2017-10-31 12:08:46 +0530

    Add files via upload

---
+++ Correct_way_to_use_3_scanfs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+printf("1st:");
+char a;
+scanf(" %c",&a);
+printf("2nd:");
+char b;
+scanf(" %c",&b);
+printf("3nd:");
+char c;
+scanf(" %c",&c);
+
+
+
+
+}
---
+++ Error_when_using_2_scanfs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+printf("1st:");
+char c;
+scanf("%c",&c);
+printf("2nd:");
+char d;
+scanf("%c",&d);
+printf("3rd:");
+char e;
+scanf("%c",&e);
+//This happens due to newline character entered when you press "ENTER" KEY
+//USE scanf(" %c") INSTEAD  as it removes trailing spaces as well as white spaces
+
+}
