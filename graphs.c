d96dd23742d59d9e873c2f6c38ef7fca07c4b8f9
debakun12 <31770244+debakun12@users.noreply.github.com>
2017-11-03 01:33:40 +0530

    Add files via upload

---
+++ graphs.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main(){
+FILE *fp;
+int qty[7];
+printf("Enter Qty:");
+int i=0;
+while(i<7){
+    scanf("%d",&qty[i]);
+    i++;
+}
+
+fp=fopen("graph.html","w");
+
+
+fprintf(fp,"%s","<html><body><canvas id=\"myCanvas\" width=\"1350\" height=\"20000\"style=\"border:1px solid #c3c3c3;\">Your browser does not support the canvas element.</canvas><script>var canvas = document.getElementById(\"myCanvas\");var ctx = canvas.getContext(\"2d\");ctx.fillStyle = \"#000000\";");
+i=0;
+while(i<7){
+fprintf(fp,"%s%d%s%d%s","ctx.fillRect(",  200*i+25, ",100,75,"  , qty[i],");");
+i++;
+}
+i=100;
+
+while(i<10000){
+
+
+fprintf(fp,"%s%d%s%d%s","ctx.font=\"10px Arial\";ctx.fillText(\"",i-100,"\",0,",i,");");
+fprintf(fp,"%s%d%s%d%s","ctx.beginPath();ctx.moveTo(0,",i,");ctx.lineTo(5000,",i,");ctx.stroke();");
+
+i=i+100;
+
+}
+fprintf(fp,"%s","</script>");
+}
+
